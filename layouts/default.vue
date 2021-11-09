060ab8b0f8dcd1f85a184ba5dbac9c16d0f23e5b
PMCP <maartenlauwaert@gmail.com>
2021-11-09 20:53:07 +0100

    made nav sticky again

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex flex-col md:flex-row flex-wrap container mx-auto lg:px-10">
       <!-- Navigation in desktop view -->
-      <Navigation class="hidden md:flex w-full h-44  top-0 z-20"/>
+      <Navigation class="hidden md:flex sticky w-full h-44  top-0 z-20"/>
       <div class="md:w-1/2 ">
         <div class="sticky top-44 sm:mr-20 lg:mr-32  ">
           <div class="text-7xl text-center border-b py-10 md:py-0 md:border-b-0 text-center md:text-left">
