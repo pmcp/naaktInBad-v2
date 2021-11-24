324e8054c668c6869fc540ccecef70ba58d70a5a
PMCP <maartenlauwaert@gmail.com>
2021-11-24 13:33:15 +0100

    added over page

--- pages/over.vue
+++ pages/over.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    OVER
+    <nuxt-content
+      :document="page"
+      class="prose mt-10"
+    />
   </div>
 </template>
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    let page
+    try {
+      page = await $content('pages', 'over').fetch()
+      // OR const article = await $content(`articles/${params.slug}`).fetch()
+    } catch (e) {
+      error({ message: 'Page "over" not found' })
+    }
+
+    return {
+      page
+    }
+  }
+}
+</script>
