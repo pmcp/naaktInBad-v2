3691b834e643e7b75b19e8fa286cce904b2a2841
PMCP <maartenlauwaert@gmail.com>
2021-11-26 15:35:25 +0100

    design player

--- TODO.md
+++ TODO.md
@@ -1,12 +1,12 @@
 #DOING
-
+- Design player
 
 #TODO
 
 
 #OPTIM
 - Use Grid, so player can move to any position (now using resizeobserver)
-
+- Move out css of player (in nuxt.config head), in favor of own css / tailwind
 #QUESTIONS
 - Two nav buttons in diff places for mobile?
 - Better icon for hamburger
--- components/Player.vue
+++ components/Player.vue
@@ -2,7 +2,7 @@
   <div>
     <div
       v-if="article"
-      class="w-full flex justify-end"
+      class="w-full flex justify-end relative top-5 z-10"
     >{{ article.song }}</div>
     <vue-plyr
       v-if="article"
@@ -22,11 +22,9 @@ export default {
   data() {
     return {
       controls: `<div class="plyr__controls flex">
-          <button type="button" class="plyr__control mr-2" aria-label="Play, {title}" data-plyr="play">
-              <svg class="icon--pressed" role="presentation"><use xlink:href="#plyr-pause"></use></svg>
-              <svg class="icon--not-pressed" role="presentation"><use xlink:href="#plyr-play"></use></svg>
-              <span class="label--pressed plyr__tooltip" role="tooltip">Pause</span>
-              <span class="label--not-pressed plyr__tooltip" role="tooltip">Play</span>
+          <button type="button" class="plyr__control mr-2 mb-6 p-2 hover:p-2 rounded-none" aria-label="Play, {title}" data-plyr="play">
+              <svg role="presentation" class="icon--pressed" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="141.73px" height="141.73px" viewBox="0 0 141.73 141.73" xml:space="preserve"><g><rect x="22" width="32.847" height="141.152"/><rect x="87.684" width="32.839" height="141.152"/></g></svg>
+              <svg role="presentation" class="icon--not-pressed" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="141.73px" height="141.73px" viewBox="0 0 141.73 141.73" xml:space="preserve"><polygon points="9.637,5.656 131.556,71.589 9.637,137.535 "/></svg>
           </button>
           <div class="flex-grow flex flex-col italic">
             <div class="text-right"></div>
@@ -35,7 +33,7 @@ export default {
                   <progress class="plyr__progress__buffer" min="0" max="100" value="0">% buffered</progress>
                 <span role="tooltip" class="plyr__tooltip">00:00</span>
             </div>
-            <div class="flex justify-end">
+            <div class="flex justify-end relative -top-2">
               <div class="plyr__time plyr__time--current mr-2" aria-label="Current time">00:00</div>
               <div class="plyr__time plyr__time--duration" aria-label="Duration">00:00</div>
             </div>
