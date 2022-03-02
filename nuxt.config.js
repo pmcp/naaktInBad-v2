241d326d03df950105c8d8fdff9a2753a5ef1b6a
PMCP <maartenlauwaert@gmail.com>
2022-03-02 14:31:04 +0100

    added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,9 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
       {
         rel: 'stylesheet',
         href: 'https://unpkg.com/vue-plyr/dist/vue-plyr.css'
